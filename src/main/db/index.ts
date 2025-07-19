11ccb966b4abfb27312145901f107ed7384321f9
codeherder <312822458@qq.com>
2025-07-19 10:42:43 +0800

    update: ignore migration

--- src/main/db/index.ts
+++ src/main/db/index.ts
@@ -3,7 +3,6 @@ import { DataSource } from 'typeorm'
 import { Photo } from './photo/photo.entities'
 import { User } from './user/user.entities'
 import { app } from 'electron'
-import { AddAliasToPhoto1750727572051 } from './migrations/1750727572051-AddAliasToPhoto'
 
 const isCLI = process.env.TYPEORM_CLI === 'true'
 const databasePath = path.join(isCLI ? 'C:/Users/xxxx/AppData/Roaming/electron-typeorm' : app.getPath('userData'), 'mwddata.db')
@@ -11,8 +10,8 @@ export default new DataSource({
   type: 'better-sqlite3',
   entities: [User, Photo],
   database: databasePath,
-  synchronize: false,
-  migrations:  [AddAliasToPhoto1750727572051], // TODO: auto generate & load migrations
+  synchronize: true,
+  migrations:  [], // TODO: auto generate & load migrations
   migrationsTableName: 'migrations',
   prepareDatabase(db) {
     db.pragma('foreign_keys = ON') // turn on foreign key constraints, the value of foreign keys must exist in the referenced table
